b214aba58d453bf2516c0a2ba13b92587d72ef1c
Noah Nemhed <noahnemhed@hotmail.com>
2023-10-25 12:56:06 +0200

    Connected to backend

--- src/App.js
+++ src/App.js
@@ -104,7 +104,7 @@ class App extends Component {
       .then(response => response.json())
       .then(data => {
         if(data){
-          return fetch('https://face-detection-api-hdw7.onrender.com/image', 
+          return fetch('https://face-detection-api-hdw7.onrender.com:10000/image', 
           {
             method: 'PUT',
             headers: {
